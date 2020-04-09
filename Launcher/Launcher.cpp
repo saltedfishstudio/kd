0ff0b070fef6411a138254ea2fb76512999e9407
saltedfishstudio <saltedfishstudio@pm.me>
2020-04-10 00:17:20 +0900

    드라이버 함수 호출 테스트

--- Launcher/Launcher.cpp
+++ Launcher/Launcher.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 
+#define VERSION_10_7_314_9802
+#include "Offset.h"
+
 #include "KeInterface.h"
-#include "data.h"
 #include "Vector3f.h"
 
-#define GET(NAME, TYPE, OFFSET) TYPE NAME = Driver.ReadVirtualMemory<TYPE>(ProcessId, OFFSET, sizeof(TYPE));
+#define GET(NAME, TYPE, OFFSET) auto NAME = Driver.ReadVirtualMemory<TYPE>(ProcessId, OFFSET, sizeof(TYPE));
+#define GET_CHAR(NAME, OFFSET, SIZE) char NAME[SIZE]; \
+for(int i = 0; i < SIZE; i++) { \
+	auto c = Driver.ReadVirtualMemory<char>(ProcessId, OFFSET + (i) * sizeof(char), sizeof(char)); \
+	if(c=='\0') { \
+		NAME[i] = ' '; \
+	} \
+	NAME[i] = c; \
+}
 
-#define GET_INT(NAME, DESC, OFFSET) GET(NAME, int, OFFSET)\
-printf_s("Found %s : %d\n", DESC, NAME);
-
-#define GET_FLOAT(NAME, DESC, OFFSET) GET(NAME, float, OFFSET)\
-printf_s("Found %s : %f\n", DESC, NAME);
-
-#define GET_SHORT(NAME, DESC, OFFSET) GET(NAME, short, OFFSET)\
-printf_s("Found %s : %d\n", DESC, NAME);
-
-#define GET_CHAR(NAME, DESC, OFFSET, SIZE) char NAME[SIZE]; \
-for(int i = 0; i < SIZE; i++) NAME[i] = Driver.ReadVirtualMemory<char>(ProcessId, OFFSET + (i) * sizeof(char), sizeof(char)); \
-printf_s("Found %s : %s\n", DESC, NAME);
 
 using namespace std;
 
@@ -30,11 +28,27 @@ Vector3f* GetPosition(DWORD GameObject);
 bool Initialize();
 bool bInit;
 
-KeInterface Driver("\\\\.\\cbl");
+KeInterface Driver(R"(\\.\cbl)");
 
 DWORD ProcessId;
 DWORD ClientAddress;
 DWORD LocalPlayer;
+
+class GameObject { };
+
+GameObject* GetFirst()
+{
+	typedef GameObject* (__thiscall* fnGetFirst)(void*);
+	auto fn = Driver.ReadVirtualMemory<fnGetFirst>(ProcessId, ClientAddress + oGetFirstObject, sizeof(fnGetFirst));
+	auto objManager = Driver.ReadVirtualMemory<void*>(ProcessId, ClientAddress + (__int32)oObjManager, sizeof(void*));
+	return fn(objManager);
+}
+
+GameObject* GetNext(GameObject * current)
+{
+	typedef GameObject* (__thiscall* fnGetNext)(void*, GameObject*);
+	return reinterpret_cast<fnGetNext>((ClientAddress + oGetNextObject))(*reinterpret_cast<void**>(ClientAddress + (__int32)oObjManager), current);
+}
 
 int main()
 {
@@ -44,22 +58,35 @@ int main()
 		Sleep(1000);
 	}
 	
-	GET_SHORT(index, "index", LocalPlayer + static_cast<__int32>(oGameObject::oObjIndex));
-	GET_INT(team, "team", LocalPlayer + static_cast<__int32>(oGameObject::oObjTeam));
-	GET_FLOAT(health, "health", LocalPlayer + static_cast<__int32>(AttackableUnit::oObjHealth));
+	GET(index, short, LocalPlayer + oObjIndex);
+	GET(Team, int, LocalPlayer + oObjTeam);
 
-	GET_CHAR(summoner, "name", LocalPlayer + 0x006C, 9);
+	GET(currentHealth, float, LocalPlayer + oAtkCurHealth);
+	GET(maxHealth, float, LocalPlayer + oAtkMaxHealth);
+
+	GET_CHAR(summoner, LocalPlayer + oObjName, 16);
+	GET_CHAR(championName, LocalPlayer + oChampionName, 16);
+	
+	printf_s("Champion : %s\n", championName);
+	printf_s("Object Name : %s\n", summoner);
+
+	auto obj = GetFirst();
+	
+	while (obj)
+	{
+		auto unit = (GameObject*)obj;
+		if (unit)
+		{
+			GET(objIndex, short, (DWORD)obj + oObjIndex);
+			printf_s("Index : %d ", objIndex);
+		}
+
+		obj = GetNext(obj);
+	}
 
 	//while (true)
 	//{
 	//	//system("cls");
-
-	//	//xx = Driver.ReadVirtualMemory<float>(ProcessId, LocalPlayer + (__int32)oGameObject::oObjPos, sizeof(float));
-	//	//yy = Driver.ReadVirtualMemory<float>(ProcessId, LocalPlayer + (__int32)oGameObject::oObjPos + sizeof(float), sizeof(float));
-	//	//zz = Driver.ReadVirtualMemory<float>(ProcessId, LocalPlayer + (__int32)oGameObject::oObjPos + sizeof(float) + sizeof(float), sizeof(float));
-
-	//	//printf_s("Position : %f %f %f", xx, yy, zz);
-	//	
 	//	/*typedef void(__cdecl* fnDrawCircle)(Vector3f*, float, int*, int, int, int, float);
 	//	reinterpret_cast<fnDrawCircle>(ClientAddress + 0x4F8C40)(new Vector3f(100, 100, 100), 500, 0, 0, 0, 0, 1);*/
 	//	
@@ -88,7 +115,7 @@ bool Initialize()
 
 	ProcessId = Driver.GetTargetPid();
 	ClientAddress = Driver.GetClientModule();
-	LocalPlayer = Driver.ReadVirtualMemory<DWORD>(ProcessId, ClientAddress + 0x349A0E4, sizeof(DWORD));
+	LocalPlayer = Driver.ReadVirtualMemory<DWORD>(ProcessId, ClientAddress + oLocalPlayer, sizeof(DWORD));
 
 	std::cout << "Found lol Process Id: " << ProcessId << std::endl;
 	std::cout << "Found League of Legends.exe: 0x" << std::uppercase << std::hex << ClientAddress << std::endl;
@@ -99,9 +126,9 @@ bool Initialize()
 
 Vector3f* GetPosition(DWORD GameObject)
 {
-	const auto xx = Driver.ReadVirtualMemory<float>(ProcessId, GameObject + static_cast<__int32>(oGameObject::oObjPos), sizeof(float));
-	const auto yy = Driver.ReadVirtualMemory<float>(ProcessId, GameObject + static_cast<__int32>(oGameObject::oObjPos) + sizeof(float), sizeof(float));
-	const auto zz = Driver.ReadVirtualMemory<float>(ProcessId, GameObject + static_cast<__int32>(oGameObject::oObjPos) + sizeof(float) + sizeof(float), sizeof(float));
+	const auto xx = Driver.ReadVirtualMemory<float>(ProcessId, GameObject + oObjPos, sizeof(float));
+	const auto yy = Driver.ReadVirtualMemory<float>(ProcessId, GameObject + oObjPos + sizeof(float), sizeof(float));
+	const auto zz = Driver.ReadVirtualMemory<float>(ProcessId, GameObject + oObjPos + sizeof(float) + sizeof(float), sizeof(float));
 
 	return new Vector3f(xx, yy, zz);
 }
---
+++ Launcher/Offset.h
@@ -0,0 +1,91 @@
+#pragma once
+//#define VERSION_10_7_314_9802
+#ifdef VERSION_10_7_314_9802
+
+/* Client */
+#define oChatClient			0x1BF5BD8
+#define oPrintChat			0x566E70
+
+/* Game Info */
+#define oGameTime			0x3492FF4
+#define oGmaeInfo			0x306C20
+#define oGameVersion		0x34A3250
+
+/* Renderer */
+#define oDrawCircle			0x4F8C40
+#define oRenderer			0x34BA384
+#define oWorldToScreen		0x92D200
+
+/* Object Manager */
+#define oLocalPlayer		0x349A0E4
+#define oObjManager			0x1BF8598
+#define oGetFirstObject		0x2AEEF0
+#define oGetNextObject		0x2B01C0
+
+/* Spell Helper */
+#define oGetAttackDelay		0x2AA730
+#define oGetAttackCastDelay	0x2AA630
+
+/* Can be banned */
+#define oIssueOrder			0x18A800
+
+/* HUD */
+#define oHudInstance		0x1BF83B4
+
+
+/* oGameObject */
+#define oObjType			0x4
+#define oObjIndex			0x20
+#define oObjTeam			0x4C
+
+#define oObjName			0x6C
+#define oObjNetworkId		0xCC
+
+#define oObjPos				0x1D8
+#define oObjVisibility		0x450
+
+/* oAttackableUnit */
+#define oAtkCurMana			0x47C
+#define oAtkMaxMana			oAtkCurMana		+ 0x10
+
+#define oAtkCurHealth		0xF88
+#define oAtkMaxHealth		oAtkCurHealth	+ 0x10
+
+#define oAtkCooldown		0x126C
+
+#define oAtkBonusAtk		0x13B4
+#define oAtkBaseAtk			0x1960
+
+#define oAtkCritical		0x1458
+
+#define oAtkArmor			0x145C
+#define oAtkBonusArmor		oAtkArmor		+ 0x4
+
+#define oAtkMagicRes		0x1464
+#define oAtkBonusMagicRes	oAtkMagicRes	+ 0x4
+
+#define oAtkMaxMoveSpeed	0x1474
+
+#define oAtkAtkRange		0x147C
+
+#define oAtkAP				0x18E0
+#define oAtkAPMultiplier	0x18F0
+
+#define oAtkMoveSpeed		0x1A50
+
+#define oAtkBuffMgr			0x2308
+#define oAtkSpellBook		0x2AB8
+
+#define oAtkLevel			0x4DEC
+
+/* Obj_AI_Base */
+#define oAIBResourceName	0x240
+
+#define oAIBGold			0x1CBC
+#define oAIBGoldTotal		oAIBGold			+ 0x10
+
+/* Obj_AI_Hero */
+#define oHeroExperience		0x4DA4
+#define oChampionName		0x3578
+
+#endif
